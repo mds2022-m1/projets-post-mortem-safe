1d757dba721ce4cb79a04023a375e24547beb34c
Marais Paul <test@test.fr>
2022-11-29 11:12:30 +0100

    feat: add DB_PORT to .env and use it on app module

--- post_mortem_safe_back/src/app.module.ts
+++ post_mortem_safe_back/src/app.module.ts
@@ -12,18 +12,18 @@ import { UsersModule } from './users/users.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type:'mysql',
+      type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: (process.env.DB_PORT as unknown) as number,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql'
+      autoSchemaFile: 'schema.gql',
     }),
     UsersModule,
   ],
